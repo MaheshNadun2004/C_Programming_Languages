f2e8d38ee83d4aa3780847a5af371b9ccb0e57f2
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-12-18 15:30:46 +0530

    Create No_02.c

---
+++ No_02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int age = 21;
+	float heigh = 5;
+	char grade = 'A';
+	printf("Age is %d . \n" , age);
+	printf("heigh is %f . \n" , heigh);
+	printf("grade is %c . \n" , grade);
+}
